303cfd09b8af8fddb461a27350b579dcd298c987
Michael Saul <michaelsaul@MacBook-Pro.local>
2020-10-16 13:49:56 -0700

    Updates

---
+++ precache-manifest.331143451b6ec1141e6bdd0fc5d2b8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8471e5dfab63099dbec8722f47c73014",
+    "url": "/mikeyvsaul/project-4-assessment.git/index.html"
+  },
+  {
+    "revision": "b70e7430fe2ed3eab719",
+    "url": "/mikeyvsaul/project-4-assessment.git/static/css/main.c0835aaf.chunk.css"
+  },
+  {
+    "revision": "179143e57da302c9cb91",
+    "url": "/mikeyvsaul/project-4-assessment.git/static/js/2.21de34d4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mikeyvsaul/project-4-assessment.git/static/js/2.21de34d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b70e7430fe2ed3eab719",
+    "url": "/mikeyvsaul/project-4-assessment.git/static/js/main.3db81851.chunk.js"
+  },
+  {
+    "revision": "4baab2995eadb2fa3965",
+    "url": "/mikeyvsaul/project-4-assessment.git/static/js/runtime-main.05dada0f.js"
+  }
+]);
